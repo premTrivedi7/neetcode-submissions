825e663f8e4242fd61b56f5580b5398e2ef24db6
premTrivedi7 <108577077+premTrivedi7@users.noreply.github.com>
2026-06-18 14:18:29 -0400

    Add: design-browser-history - submission-3

---
+++ Data Structures & Algorithms/design-browser-history/submission-3.java
@@ -0,0 +1,74 @@
+class Node{
+    String url;
+    Node next;
+    Node prev;
+    public Node(String url){
+        this.url = url;
+        this.next = null;
+        this.prev = null;
+    }
+
+}
+class BrowserHistory {
+    private Node head;
+    private Node tail;
+    private int size;
+    private int k;
+    public BrowserHistory(String homepage) {//O(1)
+        head = tail = new Node(homepage);
+        size=1;
+        k=0;
+    }
+    
+    public void visit(String url) {//     O(k/n)
+        Node newNode = new Node(url);
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+
+        Node temp = findKthElement();
+        temp.next=tail;
+        tail.prev = temp;
+        
+        k++;
+        size=k+1;
+    }
+    
+    public String back(int steps) {//   O(n)
+        Node temp = findKthElement();
+        while(steps!=0 && temp.prev!= null){
+            temp = temp.prev;
+            k--;
+            steps--;
+        }
+
+        return temp.url;
+    }
+    
+    public String forward(int steps) {  //O(n)
+        Node temp = findKthElement();
+        while(steps!=0 && temp.next!= null){
+            temp = temp.next;
+            k++;
+            steps--;
+        }
+
+        return temp.url;
+    }
+
+    private Node findKthElement(){
+        Node temp = head;
+        for(int i=1; i<=k; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
